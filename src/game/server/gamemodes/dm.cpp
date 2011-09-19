4849e37b5498b327665643b173e9090d89563c79
Pilen <fiskomaten@gmail.com>
2011-09-19 09:19:48 +0200

    Custom gamemodenames: DIKU-CTF, DIKU-DM, DIKU-TDM

--- src/game/server/gamemodes/dm.cpp
+++ src/game/server/gamemodes/dm.cpp
@@ -6,7 +6,7 @@
 CGameControllerDM::CGameControllerDM(class CGameContext *pGameServer)
 : IGameController(pGameServer)
 {
-	m_pGameType = "DM";
+	m_pGameType = "DIKU-DM";
 }
 
 void CGameControllerDM::Tick()
